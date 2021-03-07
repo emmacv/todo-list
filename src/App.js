3cdcd0c2ad62bfb2a96bd0c13f83398fa07d5596
Emmanuel Rosas <emikogears134@gmail.com>
2021-03-07 16:40:53 -0600

    first commi added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import React from 'react';
+import "./index.css";
+import TodosContainer from './Components/Molecules/TodosContainer/index'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainContainer>
+        <TodosCategories />
+        <TodosContainer />
+      </MainContainer>
+    </>
   );
 }
 
 export default App;
+
+
+const MainContainer = styled.div`
+  top: 0;
+  left: 0;
+  height: auto;
+  display: flex;
+`;
+
+const TodosCategories = styled.div`
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 20%;
+  height: 100vh;
+`;
